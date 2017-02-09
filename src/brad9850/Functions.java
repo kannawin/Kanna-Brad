ae21c1e762d53c280e788dc1850419c75f0f59c6
Christopher Bradford <chjabr@gmail.com>
2017-02-08 20:30:55 -0600

    Check for future collisions between moving objects.

--- src/brad9850/Functions.java
+++ src/brad9850/Functions.java
@@ -15,6 +15,7 @@ import spacesettlers.objects.Ship;
 import spacesettlers.objects.weapons.Missile;
 import spacesettlers.simulator.Toroidal2DPhysics;
 import spacesettlers.utilities.Movement;
+import spacesettlers.utilities.Position;
 import spacesettlers.utilities.Vector2D;
 
 public class Functions{
@@ -286,6 +287,8 @@ public class Functions{
 		//	Bottom is positive, also going from Pi (left side) to 0 (right side)
 		//	Units are radians
 		double shipOrientation = ship.getPosition().getOrientation();
+		
+		//Using the distance function here: http://math.stackexchange.com/questions/275529/check-if-line-intersects-with-circles-perimeter
 		double a = Math.tan(shipOrientation);
 		double b = -1;
 		double c = shipY - a * shipX;
@@ -321,7 +324,187 @@ public class Functions{
 		else{
 			return false;
 		}
-	} 
+	}
+	
+	/**
+	 * See if shooting now will let us hit a moving target
+	 * @param space
+	 * @param ship
+	 * @param target
+	 * @param targetEstimatedVelocity
+	 * @return
+	 */
+	public static boolean willHitMovingTarget(Toroidal2DPhysics space, Ship ship, AbstractObject target, Vector2D targetEstimatedVelocity){
+		//Get info for missile that will be created
+		Position missilePosition = ship.getPosition();
+		//Adapted from AbstractWeapon.shiftFiringWeaponLocation
+		int radiusToShift = ship.getRadius() + Missile.MISSILE_RADIUS * 2;
+		missilePosition.setX(missilePosition.getX() + (radiusToShift * Math.cos(missilePosition.getOrientation())));
+		missilePosition.setY(missilePosition.getY() + (radiusToShift * Math.sin(missilePosition.getOrientation())));
+		Vector2D missileVelocity = new Vector2D(Missile.INITIAL_VELOCITY * Math.cos(missilePosition.getOrientation()), 
+				Missile.INITIAL_VELOCITY * Math.sin(missilePosition.getOrientation()));
+		
+		double timeUntilCollision = timeUntilCollision(space, 
+														missilePosition, Missile.MISSILE_RADIUS, missileVelocity,
+														target.getPosition(), target.getRadius(), targetEstimatedVelocity);
+		
+		if(timeUntilCollision >= 0){
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * See when two objects will collide. 
+	 * Will usually be off by a tiny bit, since it treats objects as boxes, not squares, but will occasionally have a false positive for collisions.
+	 * @return Estimated time until first collision, or -1 if there shouldn't be one.
+	 */
+	public static double timeUntilCollision(Toroidal2DPhysics space,
+											Position firstObjectPosition, double firstObjectRadius, Vector2D firstEstimatedVelocity,
+											Position secondObjectPosition, double secondObjectRadius, Vector2D secondEstimatedVelocity){
+		//TODO: Adapt this other approach to a toroidal surface- should be more accurate.
+		//http://gamedev.stackexchange.com/questions/97337/detect-if-two-objects-are-going-to-crash
+		
+		//Basic idea: see when the objects will occupy the same area on the X and Y axes. If they occupy the same space on both axes at the same time, they're colliding
+		
+		double[][] xCollisions = intersectingTimespan(firstObjectPosition.getX(), firstObjectRadius, firstEstimatedVelocity.getXValue(),
+														secondObjectPosition.getX(), secondObjectRadius, secondEstimatedVelocity.getXValue(),
+														space.getWidth());
+		double[][] yCollisions = intersectingTimespan(firstObjectPosition.getY(), firstObjectRadius, firstEstimatedVelocity.getYValue(),
+														secondObjectPosition.getY(), secondObjectRadius, secondEstimatedVelocity.getYValue(),
+														space.getHeight());
+		
+		//Finding the least amount of time the objects can occupy the same x and y values before we classify it as a collision
+		//It depends on the relative speed of the missile & the size of the target
+		//Remember, this approach treats objects as squares, not circles, so this lessens the impact of that
+		Vector2D relativeVelocity = firstEstimatedVelocity.add(secondEstimatedVelocity);
+		double vx = relativeVelocity.getXValue(), vy = relativeVelocity.getYValue();
+		double relativeSpeed = vx * vx + vy * vy;
+		double maxTimeColliding = (firstObjectRadius + secondObjectRadius) / relativeSpeed;
+		
+		double magicNumber = .01; //Found by experimentation to work well 
+		double timeSpentCollidingThreshold = maxTimeColliding / magicNumber;
+		
+		double smallestTimeUntilCollision = Double.MAX_VALUE;
+		for(double[] xPeriod : xCollisions){
+			for(double[] yPeriod : yCollisions){
+				
+				//If the boxes collide
+				if( xPeriod[0] < yPeriod[1] && xPeriod[1] > yPeriod[0] ){
+					double timeSpentColliding = Math.min(xPeriod[1] - yPeriod[0], yPeriod[1] - xPeriod[0]);
+					
+					if (timeSpentColliding > timeSpentCollidingThreshold) {
+						double timeUntilCollision = Math.max(xPeriod[0], yPeriod[0]);
+						if (timeUntilCollision < smallestTimeUntilCollision) {
+							smallestTimeUntilCollision = timeUntilCollision;
+						}
+					}
+				}
+				
+			}
+		}
+		
+		if (smallestTimeUntilCollision == Double.MAX_VALUE){
+			smallestTimeUntilCollision = -1;
+		}
+		
+		return smallestTimeUntilCollision;
+	}
+
+	/**
+	 * See when two objects will collide. 
+	 * Will usually be off by a tiny bit, since it treats objects as boxes, not squares, but will occasionally have a false positive for collisions.
+	 * @return Estimated time until first collision, or -1 if there shouldn't be one.
+	 */
+	public static double timeUntilCollision(Toroidal2DPhysics space, 
+											AbstractObject firstObject, Vector2D firstEstimatedVelocity, 
+											AbstractObject secondObject, Vector2D secondEstimatedVelocity){
+		return timeUntilCollision(space, 
+									firstObject.getPosition(), firstObject.getRadius(), firstEstimatedVelocity,
+									secondObject.getPosition(), secondObject.getRadius(), secondEstimatedVelocity);
+	}
+
+	/**
+	 * See when two objects will collide. 
+	 * Will usually be off by a tiny bit, since it treats objects as boxes, not squares, but will occasionally have a false positive for collisions.
+	 * @return Estimated time until first collision, or -1 if there isn't be one.
+	 */
+	public static double timeUntilCollision(Toroidal2DPhysics space, AbstractObject firstObject, AbstractObject secondObject){
+		return timeUntilCollision(space, 
+									firstObject, firstObject.getPosition().getTranslationalVelocity(),
+									secondObject, secondObject.getPosition().getTranslationalVelocity());
+	}
+											
+	
+	/**
+	 * Get the first two periods during which two objects are occupying the same space on a single axis
+	 * @param firstPosition
+	 * @param firstRadius
+	 * @param firstVelocity
+	 * @param secondPosition
+	 * @param secondRadius
+	 * @param secondVelocity
+	 * @param spaceWidth
+	 * @return
+	 */
+	private static double[][] intersectingTimespan(double centerA, double radiusA, double velocityA, 
+													double centerB, double radiusB, double velocityB,
+													int spaceWidth){
+		double[][] timespan = {{0, 0},{0,0}};
+		
+		boolean alreadyCollided = Math.abs(centerA - centerB) < (radiusA + radiusB);
+		
+		if(velocityA == velocityB){
+			if(alreadyCollided){
+				timespan[0][1] = Double.MAX_VALUE;
+			}
+			return timespan;
+		}
+		
+		//Fastest here refers to most positive velocity
+		//So, 5 is considered faster than -10
+		boolean aIsFaster = velocityA > velocityB;
+		
+		double fastestBoxRightEdge = aIsFaster ? centerA + radiusA : centerB + radiusB;
+		double fastestBoxLeftEdge  = aIsFaster ? centerA - radiusA : centerB - radiusB;
+		double slowestBoxRightEdge = aIsFaster ? centerB + radiusB : centerA + radiusA;
+		double slowestBoxLeftEdge  = aIsFaster ? centerB - radiusB : centerA - radiusA;
+		
+
+		//If the velocity of A > B (*not the magnitude!*), 
+		//  then the collision will always involve A's right side hitting B's left side
+		double distanceBetweenInnerEdges = distanceBetweenEdges(fastestBoxRightEdge, slowestBoxLeftEdge, spaceWidth);
+		double distanceBetweenOuterEdges = distanceBetweenEdges(fastestBoxLeftEdge, slowestBoxRightEdge, spaceWidth);
+		
+		double effectiveSpeed = Math.abs(velocityA - velocityB);
+		double timeToCollide = distanceBetweenInnerEdges / effectiveSpeed;
+		double timeToStopColliding = distanceBetweenOuterEdges / effectiveSpeed;
+		
+		if(alreadyCollided){
+			//If we've already collided, then timeToStopColliding will be smaller than timeToCollide
+			timespan[0][0] = 0;
+			timespan[0][1] = timeToStopColliding;
+			timespan[1][0] = timeToCollide;
+			timespan[1][1] = timeToCollide + ((radiusA + radiusB) / spaceWidth);
+		}
+		else{
+			timespan[0][0] = timeToCollide;
+			timespan[0][1] = timeToStopColliding;
+			timespan[1][0] = timeToCollide + (spaceWidth / effectiveSpeed);
+			timespan[1][1] = timeToStopColliding + (spaceWidth / effectiveSpeed);
+		}
+		
+		return timespan;
+	}
+	
+	private static double distanceBetweenEdges(double fasterEdge, double slowerEdge, double spaceWidth){
+		double distance = slowerEdge - fasterEdge;
+		//If the faster one is to the right of the slower one, then it'll wrap through the wall
+		if(fasterEdge > slowerEdge){
+			distance = spaceWidth - distance; 
+		}
+		return distance;
+	}
 	
 	
 }
