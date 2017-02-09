ee581f532bb90e03571fae31a0040506325bf01b
Christopher Bradford <chjabr@gmail.com>
2017-02-09 14:01:29 -0600

    Add bot for testing aiming, remove DumbBaseBot

---
+++ src/brad9850/BalletBot.java
@@ -0,0 +1,148 @@
+package brad9850;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import spacesettlers.actions.AbstractAction;
+import spacesettlers.actions.DoNothingAction;
+import spacesettlers.actions.PurchaseCosts;
+import spacesettlers.actions.PurchaseTypes;
+import spacesettlers.actions.RawAction;
+import spacesettlers.clients.TeamClient;
+import spacesettlers.graphics.SpacewarGraphics;
+import spacesettlers.objects.AbstractActionableObject;
+import spacesettlers.objects.AbstractObject;
+import spacesettlers.objects.Ship;
+import spacesettlers.objects.powerups.SpaceSettlersPowerupEnum;
+import spacesettlers.objects.resources.ResourcePile;
+import spacesettlers.objects.weapons.Missile;
+import spacesettlers.simulator.Toroidal2DPhysics;
+
+/**
+ * Bot that spins in place & shoots when it's told to. Used for testing targeting code.
+ * @author Christopher Bradford
+ * 
+ */
+public class BalletBot extends TeamClient {
+	boolean shouldShoot = false;
+	boolean boost = false;
+	
+	int framesSinceLastShot = 0;
+
+	/**
+	 * 
+	 */
+	public Map<UUID, AbstractAction> getMovementStart(Toroidal2DPhysics space,
+			Set<AbstractActionableObject> actionableObjects) {
+		HashMap<UUID, AbstractAction> actions = new HashMap<UUID, AbstractAction>();
+
+		// loop through each ship
+		for (AbstractObject actionable :  actionableObjects) {
+			if (actionable instanceof Ship) {
+				Ship ship = (Ship) actionable;
+				
+				AbstractAction action = getAction(space, ship);
+				actions.put(ship.getId(), action);
+				
+			} else {
+				// it is a base.  Heuristically decide when to use the shield (TODO)
+				actions.put(actionable.getId(), new DoNothingAction());
+			}
+		} 
+		return actions;
+	}
+	
+	/**
+	 * Gets the action for our ship
+	 * @param space
+	 * @param ship
+	 * @return
+	 */
+	private AbstractAction getAction(Toroidal2DPhysics space, Ship ship) {
+		AbstractObject target = space.getAsteroids().iterator().next();
+		
+		shouldShoot = false;
+		if(Functions.willHitMovingTarget(space, ship, target, target.getPosition().getTranslationalVelocity())){
+			shouldShoot = true;
+		}
+		
+		AbstractAction newAction = new RawAction(0,1);
+		return newAction;
+	}	
+
+	@Override
+	public void getMovementEnd(Toroidal2DPhysics space, Set<AbstractActionableObject> actionableObjects) {
+	}
+
+	@Override
+	public void initialize(Toroidal2DPhysics space) {
+	}
+
+	@Override
+	public void shutDown(Toroidal2DPhysics space) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Set<SpacewarGraphics> getGraphics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	/**
+	 * Never buy anything
+	 */
+	public Map<UUID, PurchaseTypes> getTeamPurchases(Toroidal2DPhysics space,
+			Set<AbstractActionableObject> actionableObjects, 
+			ResourcePile resourcesAvailable, 
+			PurchaseCosts purchaseCosts) {
+
+		HashMap<UUID, PurchaseTypes> purchases = new HashMap<UUID, PurchaseTypes>();
+
+		return purchases;
+	}
+
+	/**
+	 * Shoot whenever we can.
+	 * 
+	 * @param space
+	 * @param actionableObjects
+	 * @return
+	 */
+	@Override
+	public Map<UUID, SpaceSettlersPowerupEnum> getPowerups(Toroidal2DPhysics space,
+			Set<AbstractActionableObject> actionableObjects) {
+		HashMap<UUID, SpaceSettlersPowerupEnum> powerUps = new HashMap<UUID, SpaceSettlersPowerupEnum>();
+		framesSinceLastShot++;
+		
+		for (AbstractActionableObject actionableObject : actionableObjects){
+			SpaceSettlersPowerupEnum powerup = SpaceSettlersPowerupEnum.FIRE_MISSILE;
+			
+			
+			//Shoot less often when we're moving fast to prevent our bullets from colliding with each other
+			//TODO: Only limit this if we're aiming in the same direction we're traveling
+			double shipSpeed = actionableObject.getPosition().getTranslationalVelocity().getMagnitude();
+			int shootingDelay = 2 + (int)((shipSpeed - 15)/15);
+			
+			//If the ship is close to going as fast as a missile, don't shoot
+			if(shipSpeed + 10 > Missile.INITIAL_VELOCITY){
+				shootingDelay = Integer.MAX_VALUE;
+			}
+			
+			boolean bulletsWontCollide = framesSinceLastShot >= shootingDelay;
+			
+			if (actionableObject.isValidPowerup(powerup) && shouldShoot && bulletsWontCollide){
+				powerUps.put(actionableObject.getId(), powerup);
+				framesSinceLastShot = 0;
+			}
+		}
+		
+		
+		return powerUps;
+	}
+
+}
