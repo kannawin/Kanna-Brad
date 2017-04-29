af7b29853bb3b82ac5886304a1cafe9b1ed08620
kannawin <scott.kannawin@ou.edu>
2017-04-29 17:34:20 -0500

    Actions

--- src/brad9850/CaptureBot.java
+++ src/brad9850/CaptureBot.java
@@ -61,6 +61,7 @@ public class CaptureBot extends TeamClient {
 	int count = 0;
 	int previousTimestep = 0;
 	
+	ArrayList<UUID> ships = new ArrayList<UUID>();
 	UUID currentShip = null;
 	/**
 	 * 
@@ -75,11 +76,10 @@ public class CaptureBot extends TeamClient {
 				Ship ship = (Ship) actionable;
 				
 				UUID tempShip = ship.getId();
-				if(tempShip != currentShip){
-					currentShip = tempShip;
-					System.out.println(currentShip);
+				if (tempShip != this.currentShip){
+					System.out.println(tempShip);
+					this.currentShip = tempShip;
 				}
-				
 
 				AbstractAction action = getAction(space, ship);
 				
