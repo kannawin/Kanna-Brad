5f525b530da38328adb995cc44808d8b6128663b
Christopher Bradford <chjabr@gmail.com>
2017-05-05 23:29:55 -0500

    Improve comparator

--- src/brad9850/PlanStateComparator.java
+++ src/brad9850/PlanStateComparator.java
@@ -6,10 +6,32 @@ public class PlanStateComparator implements Comparator<PlanState>{
 
 	@Override
 	public int compare(PlanState x, PlanState y) {
+		//First, check duration
 		if(x.totalDuration < y.totalDuration){
 			return -1;
 		}
 		if(x.totalDuration > y.totalDuration){
+			return 1;
+		}
+		//If duration is equal, check number of ships
+		if(x.shipCount > y.shipCount){
+			return -1;
+		}
+		if(x.shipCount < y.shipCount){
+			return 1;
+		}
+		//If the number of ships is equal, check number of bases
+		if(x.baseCount > y.baseCount){
+			return -1;
+		}
+		if(x.baseCount < y.baseCount){
+			return 1;
+		}
+		//If the number of bases is equal, check the total resources
+		if(x.totalResources.getTotal() > y.totalResources.getTotal()){
+			return -1;
+		}
+		if(x.totalResources.getTotal() < y.totalResources.getTotal()){
 			return 1;
 		}
 		return 0;
